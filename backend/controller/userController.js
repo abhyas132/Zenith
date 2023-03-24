9c6d0abe9813a1f53fd80b15515f7bd4e9533c93
bhagyaspatel <bhagyaspatel0911@gmail.com>
2023-03-24 16:14:32 +0530

    created user controller

---
+++ backend/controller/userController.js
@@ -0,0 +1,59 @@
+const User = require('../modals/user_modal');
+const BigPromise = require("../middleware/bigPromise");
+
+exports.c = BigPromise(async(req, res, next) => {
+    let user = req.user ;
+
+    if(!user){
+        return res.status(404).json({
+            status : 404,
+            message: "user not found",
+        })
+    }
+
+    return res.status(200).json({
+        status : 200,
+        message: "retrieved successfully",
+        user,
+    })
+})
+
+exports.c = BigPromise(async(req, res, next) => {
+    const {name, email, password ,zenCoins} = req.body;
+
+    let user = req.user ;
+
+    user = {...user, name, email, password, zenCoins} ;
+
+    await user.save() ;
+
+    return res.status(200).json({
+        status : 200,
+        message: "user details updated succesfully",
+        user,
+    });
+})
+
+exports.c = BigPromise(async(req, res, next) => {
+    let {name, email, password ,zenCoins} = req.body;
+
+    const user = await User.create({
+        name, 
+        email,
+        password,
+        zenCoins
+    });
+
+    if(!user){
+        return res.status(500).json({
+            status : 500,
+            message: "Internal error, user not created",
+        });
+    }
+
+    return res.status(200).json({
+        status : 200,
+        message: "User created succesfully",
+        user,
+    });
+})
