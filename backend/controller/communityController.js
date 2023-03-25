e1994526689c080be108dbdcf9763efd8297c4fb
bhagyaspatel <bhagyaspatel0911@gmail.com>
2023-03-25 12:38:40 +0530

    community model

--- backend/controller/communityController.js
+++ backend/controller/communityController.js
@@ -1,22 +1,9 @@
 const BigPromise = require("../middleware/bigPromise");
 const scheduleModal = require("../modals/schedule_modal");
 const taskModal = require("../modals/task");
-exports.getSchedule = BigPromise(async (req, res, next) => {
-  const schedule = await taskModal.find();
 
-  if (!schedule) {
-    return res.status(200).json({
-      status: 200,
-      message: "Schedule not found",
-      task,
-    });
-  }
-
-  return res.status(200).json({
-    status: 200,
-    message: "Schedulr retrieved successfully",
-    schedule,
-  });
+exports.createCommunityPost = BigPromise(async (req, res, next) => {
+    const user = req.user ;
 });
 
 exports.createSchedule = BigPromise((req, res) => {});
