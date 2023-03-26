cab0424bc78e5f3a69c4325501986dc14c96e717
bhagyaspatel <bhagyaspatel0911@gmail.com>
2023-03-26 06:30:07 +0530

    importing created functions

--- backend/router/user_router.js
+++ backend/router/user_router.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const {signin, getAllUser, getUser, updateUser, createUser, searchUser} = require("../controller/userController") ;
+const {signin, getAllUser, getUser, updateUser, createUser, searchUser, addFriend, getFriends} = require("../controller/userController") ;
 const {isLoggedIn} = require("../middleware/userMiddleware") ;
 
 router.route("/create/user").post(createUser);
@@ -11,5 +11,7 @@ router.route("/get/user").get(isLoggedIn, getUser);
 router.route("/all/user").get(getAllUser);
 router.route("/update/user").patch(isLoggedIn, updateUser);
 router.route("/search/user").post(searchUser);
+router.route("/add/friend").post(isLoggedIn, addFriend);
+router.route("/get/friends").get(isLoggedIn, getFriends);
 
 module.exports = router;
